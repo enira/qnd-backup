d7bfd1abb36eaa3b7c2a7784e89fc4e6614314c7
enira <robbert.lambrechts@gmail.com>
2017-04-17 20:22:29 +0200

    close #58

--- qnd/reset_db.py
+++ qnd/reset_db.py
@@ -6,10 +6,8 @@ log = logging.getLogger(__name__)
 
 if __name__ == "__main__":
     """
-    Reset database data
-
-    select datid, usename, application_name,query  from pg_stat_activity ;
-
+    Reset the database data. In case you want to reset all data.
+    Can be runned with 'python reset_db.py'
     """
     app.reset_db()
     exit()
